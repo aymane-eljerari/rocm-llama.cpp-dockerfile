9a0dafc5e8c70bd070a197f4f37904e050395fbb
aymane <m.aymane.eljerari@gmail.com>
2024-09-28 16:55:38 -0400

    modify model bind mount dir

--- container_run.sh
+++ container_run.sh
@@ -7,7 +7,7 @@ docker run --privileged --gpus all -itd --rm --group-add=video \
     --security-opt="seccomp=unconfined" --device /dev/kfd --device /dev/dri \
     --name $CONTAINER_NAME \
     -v /home/eljeraria/llm-prof-utils:/app/llm-prof-utils \
-    -v /home/eljeraria/llm-models/:/app/llama.cpp/models/mounted-models/ \
+    -v /home/eljeraria/llm-models/:/app/llama.cpp/models/ \
     $IMAGE_NAME && \
 
 docker exec -it $CONTAINER_NAME zsh
